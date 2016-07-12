e13fe6b6c89976f93baa45d8f2a1f342d2de87d0
joaovelgarcia <joao.vel@hotmail.com>
2016-07-12 18:06:27 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -4,12 +4,3 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
-function search() {
-     document.getElementById("search").style.height = "10px";
-}
-function open() {
-    document.getElementById("a").style.width = "250px";
-}
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
