bf153d149dafbcf23e4ce6e2cfeff5ce210dbf02
joaovelgarcia <joao.vel@hotmail.com>
2016-07-12 18:04:33 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+function search() {
+     document.getElementById("search").style.height = "10px";
+}
+function open() {
+    document.getElementById("a").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
